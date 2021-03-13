9719a51747585ecf93c85911e05427b53b2ae7d1
kayzer24 <dante2410@hotmail.fr>
2021-03-13 14:03:54 +0100

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyCKxCUVqf5Cdo7MxHctwbeMRuRSTe81htQ",
-	authDomain: "ecommerce-3940c.firebaseapp.com",
-	databaseURL: "https://ecommerce-3940c-default-rtdb.firebaseio.com",
-	projectId: "ecommerce-3940c",
-	storageBucket: "ecommerce-3940c.appspot.com",
-	messagingSenderId: "292184481528",
-	appId: "1:292184481528:web:3a6341970d9d5bb720250d"
+	apiKey: "apikey",
+	authDomain: "auth_domain",
+	databaseURL: "db_url",
+	projectId: "project_id",
+	storageBucket: "storage_bucket",
+	messagingSenderId: "messagingSenderId",
+	appId: "appId"
 };
 
 // Initialize Firebase
